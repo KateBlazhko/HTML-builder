f3c30f224210fa6cf0b0fd0461e9df62dbf73015
KateBlazhko <kateugnicheva@gmail.com>
2022-05-15 00:07:45 +0300

    feat: add solution task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const path = require('path')
+
+const { readdir, writeFile, readFile, unlink } = require('fs')
+
+const { mkdir } = require('fs/promises');
+
+function cleanFolder(pathFolder) {
+  readdir(pathFolder, (error, files) => {
+    if (error) return console.error(error.message);
+
+    files.forEach(file => unlink(path.join(pathFolder, file), error => {
+      if (error) return console.error(error.message);
+    }));
+
+    copyFolder();
+  })
+}
+
+function copyFolder() {
+  readdir(path.join(__dirname, 'files'), (error, files) => {
+    if (error) return console.error(error.message);
+
+    files.forEach(file => {
+      readFile(path.join(__dirname, 'files', `${file}`), (error, data) => {
+        if (error) return console.error(error.message);
+
+        writeFile(path.join(__dirname, 'files-copy', `${file}`), data, error => {
+          if (error) return console.error(error.message);
+
+          console.log(`file ${file} copied`)
+        })
+      })
+    })
+  })
+}
+
+mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
+  .then(cleanFolder(path.join(__dirname, 'files-copy')))
+  .catch(error => console.error(error.message))
 
