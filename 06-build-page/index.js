78a7699da3ddb3cef0b4f9795780ddc7ae4c406e
KateBlazhko <kateugnicheva@gmail.com>
2022-05-17 22:53:14 +0300

    feat: finish solution task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,20 +1,45 @@
 const path = require('path');
-const { readdir } = require('fs/promises');
-const { createReadStream, createWriteStream, readFile  } = require('fs');
+const { readdir, readFile, mkdir, rm } = require('fs/promises');
+const { createReadStream, createWriteStream } = require('fs');
 const readline = require('readline');
 
-// Create Html
-function readTemplate(listHtmlFiles) {
-  readFile(path.join(__dirname, 'template.html'), (error, data) => {
-    if(error) return console.error(error.message);
-    
-    let templateHtml = data.toString()
+//Create any folder
+async function createFolder(pathFolder) {
+  try {
+    await mkdir(pathFolder, { recursive: true })
+    return true
+  } catch (error) {
+    console.log(error.message)
+    return false
+  }
+}
 
-    readHTMLFiles(listHtmlFiles, templateHtml)
-      .then((mainHtml) => createIndexHtml(mainHtml))
-      .catch(error => console.error(error.message));
-    
-  })
+//Create 'index.html'
+async function readComponents(pathComponents) {
+  try {
+    const files = await readdir(pathComponents, { withFileTypes: true })
+    const listHtmlFiles =  files.filter(file => file.name.match(/.html/i))
+
+    return listHtmlFiles
+
+  } catch (error) {
+    console.error(error.message)
+  }
+  
+}
+
+async function readTemplate() {
+  try {
+    const data = await readFile(path.join(__dirname, 'template.html'))
+    const templateHtml = data.toString()
+
+    return templateHtml
+      
+  } catch (error) {
+    console.error(error.message)
+  }
+
+  
 }
 
 async function readHTMLFiles(listHtmlFiles, templateHtml) {
@@ -54,25 +79,129 @@ async function readHTMLFiles(listHtmlFiles, templateHtml) {
   
 }
 
-function createIndexHtml(mainHtml) {
-  const writeStream = createWriteStream(path.join(__dirname, 'project-dist', 'index.html'),
-    'utf-8');
+async function createIndexHtml() {
+  const listHtmlFiles = await readComponents(path.join(__dirname, 'components'))
+  const templateHtml = await readTemplate()
+  const indexHtml = await readHTMLFiles(listHtmlFiles, templateHtml);
+  
+  return new Promise ((resolve, reject) => {
+    const writeStream = createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
 
-  writeStream.write(mainHtml, error => {
-    if (error) return console.error(error.message);
-    writeStream.emit('end')
-  })
+    writeStream.write(indexHtml, error => {
+      if (error) return console.error(error.message);
+      writeStream.end();
+    })
 
-  writeStream.on('end', () => {
-    console.log('Your index.html is ready!')
+    writeStream.on('finish', () => {
+      console.log('Your index.html is ready!');
+      resolve();
+    })
+
+    writeStream.on('error', reject);
+
   })
-  writeStream.on('error', error => console.error(error.message));
+  
 }
 
-readdir(path.join(__dirname, 'components'), { withFileTypes: true })
-  .then(files => {
-    const listHtmlFiles = files.filter(file => file.name.match(/.html/i))
+//Create 'style.css'
+async function createStyleCss() {
+  try {
+    const files = await readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+     
+    const listCSSFiles = files.filter(file => {
+      if (file.isFile && file.name.match(/.css/i)) {
+        return true
+      }
+        return false
+    })
+  
+    const promisesReading = []
+      
+    listCSSFiles.forEach(file =>{
+      promisesReading.push(new Promise((resolve, reject) => {
+        const readStream = createReadStream(path.join(__dirname, 'styles', `${file.name}`))
+        const style = []
+  
+        readStream.on('data', data => style.push(data))
+        readStream.on('end', () => {
+          resolve(...style)
+        })
+        readStream.on('error', reject);
+      }))
+    })
 
-    readTemplate(listHtmlFiles)
+    Promise.all(promisesReading)
+        .then(styles  => {
+          const writeStream = createWriteStream(path.join(__dirname, 'project-dist', 'style.css'),
+            'utf-8')
+          writeStream.write(styles.join('\n'), error => {
+            if (error) return console.error(error.message);
+              writeStream.emit('end')
+            })
+          writeStream.on('end', () => {
+            console.log('Your style.css is ready!')
+          })
+          writeStream.on('error', error => console.error(error.message));
+        })
+        .catch(error => console.error(error.message));
+
+  } catch (error) {
+    console.error(error.message);
+  }  
+}
+
+//Create 'assets'
+async function createAssets() {
+  try {
+    const pathDist = path.join(__dirname, 'project-dist', 'assets');
+    const pathSrc = path.join(__dirname, 'assets')
+    await rm(pathDist, { recursive: true, force: true })
+    await copyFolder(pathDist, pathSrc)
+
+    return console.log('Your folder "assets" is ready')
+
+  } catch (error) {
+    console.error(error.message);
+  }
+ 
+}
+
+async function copyFolder(pathDist, pathSrc) {
+  try {
+    await createFolder(pathDist);
+
+    const files = await readdir(pathSrc, { withFileTypes: true})
+
+    files.forEach(file => {
+
+      if(file.isFile()) {
+        const readStream = createReadStream(path.join(pathSrc, `${file.name}`))
+        const writeStream = createWriteStream(path.join(pathDist, `${file.name}`))
+
+        readStream.pipe(writeStream)
+
+        readStream.on('error', error => console.error(error.message))
+        writeStream.on('error', error => console.error(error.message))
+        
+      } else {
+        copyFolder(path.join(pathDist, `${file.name}`), path.join(pathSrc, `${file.name}`),)
+      }
+      
+    })
+
+  } catch (error) {
+    console.error(error.message);
+  } 
+}
+
+
+
+createFolder(path.join(__dirname, 'project-dist'))
+  .then(async (isCreate) => {
+    if (isCreate) {
+      createIndexHtml();
+      createStyleCss();
+      createAssets();
+    }
   })
   .catch(error => console.error(error.message));
